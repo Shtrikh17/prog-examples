c74d014d41fb98c5c6ad69af562864fd65f3e05a
Ilya Shilov <ilia.shilov@yandex.ru>
2020-11-18 17:25:45 +0300

    Create clienc.cpp

---
+++ c++/network/boost::asio/sync-client-server/clienc.cpp
@@ -0,0 +1,6 @@
+// Простой пример клиента
+using boost::asio;
+io_service service;
+ip::tcp::endpoint ep( ip::address::from_string("127.0.0.1"), 2001);
+ip::tcp::socket sock(service);
+sock.connect(ep);
